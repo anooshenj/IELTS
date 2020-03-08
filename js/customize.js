571d3b7ca074418910e3fda6765cbff6e132e2c0
Anooshe Nejatian <somayeh.nejatian@gmail.com>
2020-03-08 18:45:48 +0400

    Init IELTS Project.

---
+++ js/customize.js
@@ -0,0 +1,24 @@
+
+
+jQuery(document).ready(function($){
+    $('.sliderfa').each(function(){
+     $(".slider-items").slick({
+
+     autoplay:true,
+     autoplayspeed:4500,
+     dots:true,
+     slidesToShow:1,
+     slidesToScroll:1,
+
+     });
+
+
+     });
+
+
+
+
+
+    });
+
+
